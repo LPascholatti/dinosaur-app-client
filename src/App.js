d173926c472c22f568b4614f6bc9afbc24268a85
LPascholatti <lucaspascholatti@gmail.com>
2019-10-30 08:34:16 +0100

    Remove unused import in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import 'typeface-roboto';
-//import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
 import NavBar from './components/NavBar' 
 
 function App() {
