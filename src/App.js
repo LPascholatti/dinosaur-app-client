e0e9c929e33149bb60b40edbe806cd7279959784
LPascholatti <lucaspascholatti@gmail.com>
2019-10-30 08:32:46 +0100

    Include NavBar component and import in App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import 'typeface-roboto';
 //import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+import NavBar from './components/NavBar' 
 
 function App() {
   return (
     <div className="App">
-      <h1 id="Title">Dino App</h1>
+      <NavBar />
     </div>
   );
 }
